cd9bf96aea3da2458a071ad426c5314cdaf0e7a2
jagannathbehera312 <jagannathbehera312@gmail.com>
2022-06-05 00:53:22 +0530

    Updation

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ class NewsApp:
         self.root = Tk()
         self.root.geometry('350x600')
         self.root.resizable(0,0)
-        self.root.title('Mera News App')
-        self.root.configure(background='black')
+        self.root.title('Inshorts Clone')
+        self.root.configure(background='white')
 
     def clear(self):
         for i in self.root.pack_slaves():
@@ -31,6 +31,11 @@ class NewsApp:
 
         # clear the screen for the new news item
         self.clear()
+
+        heading = Label(self.root, text=self.data['articles'][index]['title'], bg='white', fg='black', wraplength=350,
+                        justify='center')
+        heading.pack(pady=(10, 20))
+        heading.config(font=('verdana', 15))
 
         # image
         try:
@@ -45,31 +50,32 @@ class NewsApp:
             photo = ImageTk.PhotoImage(im)
 
 
-        label = Label(self.root,image=photo)
+        label = Label(self.root,image=photo,bg='white')
         label.pack()
 
 
-        heading = Label(self.root,text=self.data['articles'][index]['title'],bg='black',fg='white',wraplength=350,justify='center')
-        heading.pack(pady=(10,20))
-        heading.config(font=('verdana',15))
 
-        details = Label(self.root, text=self.data['articles'][index]['description'], bg='black', fg='white', wraplength=350,justify='center')
+
+        details = Label(self.root, text=self.data['articles'][index]['description'], bg='white', fg='green', wraplength=350,justify='center')
         details.pack(pady=(2, 20))
         details.config(font=('verdana', 12))
 
-        frame = Frame(self.root,bg='black')
+        frame = Frame(self.root,bg='white')
         frame.pack(expand=True,fill=BOTH)
 
         if index != 0:
-            prev = Button(frame,text='Prev',width=16,height=3,command=lambda :self.load_news_item(index-1))
-            prev.pack(side=LEFT)
+            prev = Button(frame,text='Prev',width=13,height=3,bg='#d9936a',fg='#004aad',command=lambda :self.load_news_item(index-1))
+            prev.pack(side=LEFT,padx=(10,0))
 
-        read = Button(frame, text='Read More', width=16, height=3,command=lambda :self.open_link(self.data['articles'][index]['url']))
-        read.pack(side=LEFT)
+        read = Button(frame, text='Read More', width=13, height=3,bg='#d9936a',fg='#004aad',command=lambda :self.open_link(self.data['articles'][index]['url']))
+        read.pack(side=LEFT,padx=(10,0))
 
         if index != len(self.data['articles'])-1:
-            next = Button(frame, text='Next', width=16, height=3,command=lambda :self.load_news_item(index+1))
-            next.pack(side=LEFT)
+            next = Button(frame, text='Next', width=13, height=3,bg='#d9936a',fg='#004aad',command=lambda :self.load_news_item(index+1))
+            next.pack(side=LEFT,padx=(10,0))
+
+        labela = Label(self.root, text='CREATED BY JAGANNATH BEHERA')
+        labela.pack()
 
         self.root.mainloop()
 
